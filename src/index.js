cc68d853d32148f9feb7e70fe421b9403537ac04
Sumanyu Aggarwal <67601246+SuPythony@users.noreply.github.com>
2021-10-27 12:05:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCcPh2TlhShi2S-fMQHgifo5qWmNLAWzoY",
-  authDomain: "notesapp-958ed.firebaseapp.com",
-  projectId: "notesapp-958ed",
-  storageBucket: "notesapp-958ed.appspot.com",
-  messagingSenderId: "317214101303",
-  appId: "1:317214101303:web:66b9565e963c69540ad4b2",
-  measurementId: "G-P6PR8HYTJB"
-};
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
